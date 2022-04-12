8f1459bfa0c065c1948283826931f08ceddf5448
audumbarasm2 <audumbarasm2@gmail.com>
2022-04-12 23:27:56 +0530

    I have updated in test code

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,6 +1,6 @@
 public class Test {
 
     public static void main(String[] args) {
-        System.out.println("Hello world, This is my first integration with git");
+        System.out.println("Hello world, This is my first integration with git now I am testing auto build");
     }
 }
